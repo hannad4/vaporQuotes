82cbec5597a7f443ec5c997e473f909f02f57560
Danny Hanna <hannad4@tcnj.edu>
2022-09-17 13:18:44 -0400

    final font change and readme add

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 import './style.css'
 import * as three from 'three'
-import { Color } from 'three';
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 import * as Quote from 'quotesy'; 
 
 const scene = new three.Scene(); 
@@ -25,7 +22,6 @@ const random_hex_color_code = () => {
 }; 
 
 
-// const Quote = require('random-quotes'); 
 const planeGeometry = new three.PlaneGeometry(window.innerWidth, window.innerHeight, 100, 100);  
 const planeMaterial = new three.MeshStandardMaterial({color: random_hex_color_code()/*0xf0b46a*/ , wireframe:true, wireframeLinewidth:50}); 
 const plane = new three.Mesh(planeGeometry,planeMaterial); 
@@ -67,12 +63,12 @@ scene.background = vaporWaveTexture;
 
 var quoteHTMLElement = document.createElement('div');
 quoteHTMLElement.style.position = 'absolute';
-//text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
-quoteHTMLElement.style.width = 100;
-quoteHTMLElement.style.height = 100;
-quoteHTMLElement.style.backgroundColor = "blue";
-quoteHTMLElement.style.top = 200 + 'px';
-quoteHTMLElement.style.left = 200 + 'px';
+quoteHTMLElement.style.fontFamily = 'Verdana'; 
+quoteHTMLElement.style.fontSize = '20px'; 
+quoteHTMLElement.style.width = 50;
+quoteHTMLElement.style.height = 75;
+quoteHTMLElement.style.top = 150 + 'px';
+quoteHTMLElement.style.left = 150 + 'px';
 
 const animate = () => {
 
@@ -84,59 +80,17 @@ const animate = () => {
   plane3.position.z = ((elapsedTime * 9) % 200) - 3 ; 
   plane4.position.z = ((elapsedTime * 9) % 200) - 4 ; 
   plane5.position.z = ((elapsedTime * 9) % 200) - 5 ; 
-  // console.log(Quote.getRandomQuote()); 
-
-  // if () {
-  //   setTimeout(function() {
-  //     console.log(Quote.getRandomQuote()); 
-  //     console.log(" "); 
-  //   }, 3000); 
-  // }
-
-  // do {
-  //   console.log(Quote.getRandomQuote()); 
-  // }
-  // while (plane.position.z == ((elapsedTime * 9) % 200));
 
   requestAnimationFrame(animate);
-
-  // setTimeout(function() {
-  //   plane.material.color.setHex(random_hex_color_code());
-  // }, delayInMilliseconds);
-
-  
 }; 
 
 function pullQuote() {
   plane.material.color.set(random_hex_color_code())
   let quoteNAuthor = Quote.random();
-  console.log(quoteNAuthor.text + "\n-" + quoteNAuthor.author); 
-  quoteHTMLElement.innerHTML = quoteNAuthor.text;
+  quoteHTMLElement.innerHTML = quoteNAuthor.text + "<br></br>-" + quoteNAuthor.author;
   document.body.appendChild(quoteHTMLElement);
   setTimeout(pullQuote, 30000);
 }
 
 pullQuote();
 animate(); 
- 
-
-// document.querySelector('#app').innerHTML = 
-// `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
